014f8ecd53083f1e04e41525c0ebf59fbc2c7407
Modymanour <modymansour2004@gmail.com>
2026-07-30 20:35:27 +0300

    Tried to configure swagger but alternatively switched to postman and tested first endpoint

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const express = require('express');
 const app  = require('express')();
-const PORT = 8080;
-app.listen();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.listen(
+    PORT,
+    () => console.log(`server is running on Port : ${PORT}`)
+);
+
+app.post('/hello', (req, res) => {
+    if(!req.body.name){
+        return res.status(400).json({ error: "Name is required"});
+    }
+    const { name } = req.body;
+    res.json({ message: `Hello, ${name}!` });
+})
