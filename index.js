bf2ce6bac5a8d3d6ab627e33a2df36282a5a1ae4
Modymanour <modymansour2004@gmail.com>
2026-07-30 20:55:57 +0300

    Stage 2: read endpoints with 404

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app  = require('express')();
 const PORT = 3000;
 
+var tasks = [
+    { id: 1, title: "Task 1", done: false },
+    { id: 2, title: "Task 2", done: false },
+    { id: 3, title: "Task 3", done: false },
+];
+
 app.use(express.json());
 
 app.listen(
@@ -23,4 +29,18 @@ app.get('', (req, res) => {
 
 app.get('/health', (req, res) => {
     res.send({"status" : "ok"})
+})
+
+app.get('/tasks', (req, res) => {
+    // res.json(tasks);
+    res.status(200).send({status: "success", data: tasks});
+});
+
+app.get('/tasks/:id', (req, res) => {
+    const taskId = parseInt(req.params.id);
+    const task = tasks.find(t => t.id === taskId);
+    if (!task) {
+        return res.status(404).json({ error: "Task not found" });
+    }
+    res.status(200).json({ status: "success", data: task });
 })
